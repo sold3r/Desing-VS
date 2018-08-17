7489dab8f1787f24165de3d88323a5266f350ae0
sold3r <efran089@gmail.com>
2018-08-17 23:39:26 +0200

    move panel

--- DesingApplication/DesingApplication/Form1.cs
+++ DesingApplication/DesingApplication/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace DesingApplication
 {
@@ -16,6 +17,12 @@ namespace DesingApplication
         {
             InitializeComponent();
         }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
 
         private void btnSlide_Click(object sender, EventArgs e)
         {
@@ -51,6 +58,12 @@ namespace DesingApplication
         private void iconMinimizar_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panelBarraTitulo_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112,0xf012,0);
         }
     }
 }
